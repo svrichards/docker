fcca41db54ba7e139310df2e871abda09d1d1400
Stephen Richards <stephen@richardsnet.us>
2021-10-20 17:00:17 -0400

    first commit

---
+++ ubuntu_setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Update
+apt-get update -y
+apt-get upgrade -y
+
+#Docker Install
+apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+groupadd docker
+sudo usermod -aG docker lcladm
+
+#Install Docker Compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#Install Portainer
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:latest
+
+#Install SSH Server
+apt-get install openssh-server
+#apt-get install openssh-server
+#nano /etc/ssh/sshd_config
+#
+
+#Set timezone
+timedatectl set-timezone America/New_York
+
+#Install Proxmox agent
+apt-get install qemu-guest-agent
+
+
